b168cbe8f6ca7cde3913330ec553645bd6ad3e98
NIharikaGhimire <niharikaghimire29@gmail.com>
2023-11-27 10:32:25 +0545

    admin and user login separate dashboard

--- random_site/database/migrations/2014_10_12_000000_create_users_table.php
+++ random_site/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('usertype')->default(0);
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -24,6 +27,8 @@ return new class extends Migration
         });
     }
 
+    // 0-> normal user_error
+    // 1-> admin
     /**
      * Reverse the migrations.
      */
